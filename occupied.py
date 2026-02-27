69dafea5cb619ee6236da16cefac16b45fe7eeb1
ErkinA25 <erkina2@nycstudents.net>
2026-02-27 10:15:00 -0500

    Create occupied.py

---
+++ occupied.py
@@ -0,0 +1,13 @@
+counter = 0
+def occupied(parking_spaces, y, t):
+    int(parking_spaces)
+    char_list = list(y)
+    char_list2 = list(t)
+    number_of_spots_that_remained_occupied = 0
+    for char_list() in char_list2():
+        while counter <= 8:
+            counter += 1
+            char_list += 1
+            number_of_spots_that_remained_occupied += 1
+    print(number_of_spots_that_remained_occupied)
+occupied(8, CCCC...., C.C.C.C.)
