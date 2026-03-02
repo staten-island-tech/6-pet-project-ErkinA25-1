8a09f45f288f95361192cd17561c4e97342a27ef
ErkinA25 <erkina2@nycstudents.net>
2026-03-02 09:52:50 -0500

    Update occupied.py

--- occupied.py
+++ occupied.py
@@ -1,13 +1,31 @@
-counter = 0
+"""
 def occupied(parking_spaces, y, t):
     int(parking_spaces)
     char_list = list(y)
     char_list2 = list(t)
     number_of_spots_that_remained_occupied = 0
-    for char_list() in char_list2():
-        while counter <= 8:
-            counter += 1
-            char_list += 1
+    for i in range(len(char_list)):
+        if char_list[i] == char_list2[i]:
             number_of_spots_that_remained_occupied += 1
     print(number_of_spots_that_remained_occupied)
-occupied(8, CCCC...., C.C.C.C.)
+occupied(8,"CCCCC...", "C.C.C.C.")
+"""
+
+language = ""
+lines_to_be_expected = input("How many lines of text are you going to input?: ")
+language_sample_text = input("Put your text here if you want its language to be determined: ")
+x  = list(language_sample_text)
+def eng_or_fren_determiner():
+    s_count = 0
+    t_count = 0
+    for char in x:
+        if char == "s" or char == "S":
+            s_count += 1
+        elif char == "t" or char == "T":
+            t_count += 1
+    if s_count >= t_count:
+        language = "French"
+    elif t_count > s_count:
+        language = "English"
+    print(language)
+eng_or_fren_determiner()
